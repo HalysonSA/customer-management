30c65d3caa85bd1a6375430fcad2e7b812b080e1
HalysonSA <hallyclash@gmail.com>
2024-11-17 00:58:44 -0300

    feat: add global error handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalErrorFilter } from './exceptions/global.error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalErrorFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
