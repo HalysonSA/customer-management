0e91cb094af3aadd3504fcbe6e7a67eeefdca413
HalysonSA <hallyclash@gmail.com>
2024-11-18 00:15:13 -0300

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './modules/customer/customer.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [CustomerModule, AuthModule],
+  imports: [CustomerModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
